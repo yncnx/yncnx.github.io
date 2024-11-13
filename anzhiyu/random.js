2eae6eb1c9484a67ef184f8f5d39e6fa91440064
yncnx <yncnx@users.noreply.github.com>
2024-11-13 03:41:01 +0000

    deploy: yncnx/hexo@f0c4d3c6f05a3f62435134c340922f12289a9437

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,3 @@
-var posts=["posts/91851bc6.html","posts/c71308be.html","posts/d04fe4f1.html","posts/b8d26614.html","posts/588ed204.html","posts/9a435a5a.html","posts/a1c374f3.html","posts/bf2f1bbe.html","posts/ca093fb9.html","posts/d224e079.html","posts/a839bc6b.html","posts/d2ab7f2a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/91851bc6/","posts/c71308be/","posts/d04fe4f1/","posts/b8d26614/","posts/588ed204/","posts/9a435a5a/","posts/a1c374f3/","posts/bf2f1bbe/","posts/ca093fb9/","posts/d224e079/","posts/a839bc6b/","posts/d2ab7f2a/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
