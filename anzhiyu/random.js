b81aab2d02c0e54dd16087249da505322c2412a7
yncnx <yncnx@users.noreply.github.com>
2026-07-29 08:30:34 +0000

    deploy: yncnx/hexo@4bb610888b4bbd19985afb7f08668a071d44fef6

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/91851bc6/","posts/c71308be/","posts/d04fe4f1/","posts/588ed204/","posts/9a435a5a/","posts/b8d26614/","posts/a1c374f3/","posts/bf2f1bbe/","posts/ca093fb9/","posts/d224e079/","posts/a839bc6b/","posts/d2ab7f2a/","posts/4a17b156/"];function toRandomPost(){
+var posts=["posts/4a17b156/","posts/91851bc6/","posts/d04fe4f1/","posts/b8d26614/","posts/c71308be/","posts/9a435a5a/","posts/a1c374f3/","posts/d2ab7f2a/","posts/588ed204/","posts/bf2f1bbe/","posts/ca093fb9/","posts/a839bc6b/","posts/d224e079/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
